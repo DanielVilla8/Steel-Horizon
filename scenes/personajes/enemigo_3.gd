119a11e1e7acea9e64be1bc2bcc386a0191c78a0
Jose Melendez <jm211424@gmail.com>
2026-07-24 22:16:14 -0600

    Version 2.3

--- scenes/personajes/enemigo_3.gd
+++ scenes/personajes/enemigo_3.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
-@onready var special_collision: CollisionPolygon2D = $Area2D/specialCollision1n
+@onready var special_collision: CollisionPolygon2D = $Area2D/SpecialCollision
 
-@export var speed: float = 200.0
+@export var speed:float =200.0
 @export var waypoints: Array[Marker2D]
 
 
@@ -17,30 +17,32 @@ func _physics_process(delta):
 	if abs(direction.x) > abs(direction.y):
 		if direction.x > 0:
 			$AnimatedSprite2D.play("right")
-			$Area2D.rotation_degrees = 0
-			$Area2D.position = Vector2(3, 0)
+			$Area2D.rotation_degrees = 180
+			$Area2D.position = Vector2(6, 0)
 		else:
 			$AnimatedSprite2D.play("left")
 			$Area2D.rotation_degrees = 180
-			$Area2D.position = Vector2(-0, 0)
+			$Area2D.position = Vector2(0, 0)
 	else:
-			if direction.y > 0:
-				$AnimatedSprite2D.play("above")
-				$Area2D.rotation_degrees = 90
-				$Area2D.position = Vector2(0, -8)
-			else:
-				$AnimatedSprite2D.play("below")
-				$Area2D.rotation_degrees = -90
-				$Area2D.position = Vector2(0, 0)
+		if direction.y > 0:
+			$AnimatedSprite2D.play("above")
+			$Area2D.rotation_degrees = -90
+			$Area2D.position = Vector2(4, -2)
+		else:
+			$AnimatedSprite2D.play("below")
+			$Area2D.rotation_degrees = 90
+			$Area2D.position = Vector2(4, 1)
+			
 	velocity = direction * speed
 	
 	if distance < min_distance:
 		current_index += 1
 		if current_index >= waypoints.size():
 			current_index = 0
-			
+				 
 	move_and_slide()
-		
+
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print("Detecto:", body.name)
 	
@@ -48,4 +50,3 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		print("¡Jugador Detectado!")
 		set_physics_process(false)
 		special_collision.change_color(Color(Color.RED, 0.3))
-	
